0cc73f14d233ef0bae9b7c4f54bf7bc2d8506ef1
fabio4520 <fabioleofc@gmail.com>
2022-09-20 23:32:30 -0500

    ADD create and new method for article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,26 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(permitted_params)
+    if @article.valid?
+      @article.save
+      flash[:notice] = "Article created successfully pes mano uwu"
+      redirect_to articles_path
+    else
+      flash[:alert] = @article.errors.full_messages.join(', ')
+      render 'articles/new'
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
