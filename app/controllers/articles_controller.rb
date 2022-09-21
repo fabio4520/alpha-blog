88ef1d071d91c479c977fb4fa28efbc6f2209f61
fabio4520 <fabioleofc@gmail.com>
2022-09-21 12:02:38 -0500

    ADD: crud method for articles controller Problem * need to add crud methods for articles controller Solution * Add manually all crud methods for articles controller, also refactoring some methods.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
   
   def index
     @articles = Article.all
@@ -8,15 +9,38 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def show; end
+
   def create
     @article = Article.new(permitted_params)
-    if @article.valid?
-      @article.save
+    if @article.save
       flash[:notice] = "Article created successfully pes mano uwu"
+      redirect_to @article
+    else
+      flash[:notice] = "Pipipi"
+      flash[:alert] = @article.errors.full_messages.join(', ')
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @article.update(permitted_params)
+      flash[:notice] = "Article updated successfully pes mano uwu"
+      redirect_to @article
+    else
+      flash[:notice] = "Pipipi"
+      flash[:alert] = @article.errors.full_messages.join(', ')
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @article.delete
       redirect_to articles_path
     else
       flash[:alert] = @article.errors.full_messages.join(', ')
-      render 'articles/new'
     end
   end
 
@@ -24,6 +48,10 @@ class ArticlesController < ApplicationController
 
   def permitted_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 
 end
